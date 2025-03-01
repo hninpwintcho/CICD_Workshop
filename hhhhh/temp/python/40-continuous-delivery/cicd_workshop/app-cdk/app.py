ff08690ed56a2711cb419944bda5c31d8d1e4297
hninpwintcho <hp.cho@nex4.net>
2025-03-01 19:11:43 +0000

    Application stack

---
+++ hhhhh/temp/python/40-continuous-delivery/cicd_workshop/app-cdk/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+
+from app_cdk.app_cdk_stack import AppCdkStack
+from app_cdk.pipeline_cdk_stack import PipelineCdkStack
+from app_cdk.ecr_cdk_stack import EcrCdkStack
+
+app = cdk.App()
+
+ecr_stack = EcrCdkStack(
+    app,
+    'ecr-stack'
+)
+
+test_app_stack = AppCdkStack(
+    app,
+    'test-app-stack',
+    ecr_repository = ecr_stack.ecr_data
+)
+
+prod_app_stack = AppCdkStack(
+    app,
+    'prod-app-stack',
+    ecr_repository = ecr_stack.ecr_data
+)
+
+pipeline_stack = PipelineCdkStack(
+    app,
+    'pipeline-stack',
+    ecr_repository = ecr_stack.ecr_data,
+    test_app_fargate = test_app_stack.ecs_service_data,
+    prod_app_fargate = prod_app_stack.ecs_service_data,
+)
+
+app.synth()
