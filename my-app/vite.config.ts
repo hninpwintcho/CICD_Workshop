3903cfed6a80b8adce6fc3f20cec7c218767c4ce
hninpwintcho <hp.cho@nex4.net>
2025-03-01 07:25:01 +0000

    new pipeline

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,9 +1,14 @@
-server: {
-  host: true,
-  port: 8081,
-  strictPort: false,  // Allow it to try another port if needed
-  allowedHosts: [
-    'test-a-testf-4ktox7iaydce-2128511845.us-east-2.elb.amazonaws.com',
-    'd24srs7fuujdui.cloudfront.net'
-  ]
-},
+/// <reference types='vite/client' />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),],
+  base: '/my-app',
+  server: {
+    host: true,
+    port: 8081,
+  },
+})
